bd5269bf128a8526d0b772014331e01734cdb6dc
hose Joo <hose0728@naver.com>
2024-09-25 00:02:41 +0900

    presentation Layer 1 까지 완료

---
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -0,0 +1,39 @@
+package sample.cafekiosk.spring.domain.product;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTypeTest {
+
+    @Test
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    void containsStockType() {
+
+        // given
+        ProductType type = ProductType.HANDMADE;
+
+        // when
+        boolean result = ProductType.containsStockType(type);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    void containsStockType2() {
+
+        // given
+        ProductType type = ProductType.BAKERY;
+
+        // when
+        boolean result = ProductType.containsStockType(type);
+
+        // then
+        assertThat(result).isTrue();
+    }
+}
